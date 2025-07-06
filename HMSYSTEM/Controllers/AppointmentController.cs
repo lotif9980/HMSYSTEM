85fee1c945b40184d17971d557e6e63f1b76c63a
Dell <Dell@DESKTOP-8OJ3TK4>
2025-07-06 23:07:09 +0600

    Appointment View Complete

--- HMSYSTEM/Controllers/AppointmentController.cs
+++ HMSYSTEM/Controllers/AppointmentController.cs
@@ -20,5 +20,49 @@ namespace HMSYSTEM.Controllers
            var appointments= _unitofWork.AppointmentRepository.GetAllAppointments();
             return View(appointments);
         }
+
+        [HttpGet]
+        public IActionResult Save()
+        {
+            var department =_unitofWork.departmentRepo.getAll()
+                .Where(c => c.Status == true)
+                .ToList();
+            var doctor = _unitofWork.doctorRepo.getAll().
+                Where(c => c.Status == true)
+                .ToList();
+
+            ViewBag.Department = department;
+            ViewBag.Doctor = doctor;
+
+
+            var lastSerial = _unitofWork.AppointmentRepository.GetAllAppointments()
+            .OrderByDescending(a => a.SerialNumber)
+            .Select(a => a.SerialNumber)
+            .FirstOrDefault();
+
+            int nextSerial = (lastSerial ?? 0) + 1; 
+            ViewBag.NextSerial = nextSerial;
+
+            return View();
+        }
+
+
+
+
+
+        [HttpGet]
+        public IActionResult GetPatientNameByPhone(string phoneNumber)
+        {
+            var patient = _unitofWork.PatienRepo.getAll()
+                .FirstOrDefault(p => p.Phone == phoneNumber && p.Status == true);
+
+            if (patient != null)
+            {
+                return Json(new { success = true, name = patient.FirstName+" " + patient.LastName });
+            }
+
+            return Json(new { success = false });
+        }
+
     }
 }
--- HMSYSTEM/Models/Appointment.cs
+++ HMSYSTEM/Models/Appointment.cs
@@ -5,9 +5,8 @@
         public int AppointmentId { get; set; }
         public string? PatientPhoneNumber { get; set; }
         public int? DepartmentId { get; set; }
-        public int? ScheduleId { get; set; }
         public int? DoctorId { get; set; }
-        public DateTime? AppoinmentDate { get;set;}
+        public DateTime? AppoinmentDate { get; set; }
         public int? SerialNumber { get; set; }
         public string ? Problem { get; set; }
         public bool Status { get; set; }
