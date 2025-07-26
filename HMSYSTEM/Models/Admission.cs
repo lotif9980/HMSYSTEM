6b703d30858f7f42a3c99e0a9a9803c91839e5ca
MdLotif <lotif.oct@gmail.com>
2025-07-26 16:02:54 +0600

    OK

---
+++ HMSYSTEM/Models/Admission.cs
@@ -0,0 +1,13 @@
+﻿namespace HMSYSTEM.Models
+{
+    public class Admission
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public int BedId { get; set; }
+        public DateTime AdmitDate { get; set; }
+        public int Status  { get; set;}
+        public DateTime CreateDate {  get; set; }
+    }
+}
